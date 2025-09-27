bc0b5261b1ede984f179a6c1f7021ac9c148cd30
BR3XALITY <marioplays254@gmail.com>
2025-09-26 20:49:14 -0700

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(function(tab) {
-    chrome.windows.create({
-        url: 'https://nomore.br3xality.org/',
-        type: 'popup',
-        width: 978,
-        height: 550
-    });
-});
